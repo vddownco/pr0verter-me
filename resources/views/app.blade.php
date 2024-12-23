8ad8390e8a88db8813b168e8e397af0295d9791a
Marcel Wagner <info@marcelwagner.dev>
2024-12-23 21:11:06 +0100

    Add Plausible (#14)

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0"/>
     <link rel="icon" type="image/x-icon" href="/favicon.ico">
     <link rel="icon" sizes="32x32 16x16" href="/favicon.ico">
+    <script defer data-domain="pr0verter.de" src="https://plausible.marcelwagner.dev/js/script.js"></script>
     @routes
     @vite('resources/js/app.js')
     @inertiaHead
