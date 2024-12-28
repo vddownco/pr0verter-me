f98a8c9d42d3ec536bb4dedb346bcfb212576384
Marcel <marceldjnairo@gmail.com>
2024-12-28 20:37:08 +0100

    WIP

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -19,6 +19,7 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
         $middleware->validateCsrfTokens(except: [
             'logout',
+            'livewire/update'
         ]);
     })
     ->withSchedule(function (Schedule $schedule) {
